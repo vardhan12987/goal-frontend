0f8a11261509afc4f326acf62294eeaaf1f34406
vardhan12987 <vardhannagireddy5@gmail.com>
2026-04-02 19:32:16 +0530

    Update vite config and add env production

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      }
-    }
+  build: {
+    outDir: 'dist'
   }
 })
